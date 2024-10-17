01962ca0a342b0b008933be9bf8d58af30b99d91
EIlif Johansen <eilifjohansen@outlook.com>
2024-10-17 13:25:20 +0200

    Fjernet Amplitude kode

--- src/proxy/route.rs
+++ src/proxy/route.rs
@@ -3,8 +3,6 @@ use std::fmt::{self, Display, Formatter};
 #[derive(Debug, PartialEq)]
 pub enum Route {
 	Umami(String),
-	Amplitude(String),
-	AmplitudeCollect(String),
 	Unexpected(String), //Someone did a goof
 }
 
@@ -17,22 +15,8 @@ impl Display for Route {
 pub fn match_route(path: String) -> Route {
 	if path.starts_with("/umami") {
 		Route::Umami(path)
-	} else if path.starts_with("/collect-auto") {
-		Route::AmplitudeCollect(path)
-	} else if path.starts_with("/collect") {
-		Route::Amplitude(path)
 	} else {
 		Route::Unexpected(path) // LIKE THIS
 	}
 }
 
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn show_starts_with() {
-		assert_eq!(
-			"/collect".starts_with("/collect"),
-			"/collect-auto".starts_with("/collect")
-		);
-	}
-}
